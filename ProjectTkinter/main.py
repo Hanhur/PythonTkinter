7ac31a7bb82ce922814880043b327c36a37f88e8
Vladimír Hanhur <aqgz@email.cz>
2024-05-02 00:03:43 +0200

    update

---
+++ ProjectTkinter/main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+# Okno
+window = Tk()
+window.title("Change")
+window.minsize(width=500, height=500)
+window.resizable(False, False)
+window.iconphoto(False, PhotoImage(file="icons/icon.png"))
+window.config(bg="#0c1f62")
+
+# Label
+greet_label = Label(window, text="EUR", bg="black", fg="white", font=("Helvetica", 18, "bold"), borderwidth=5, relief="sunken")
+greet_label.pack(pady=50)
+
+greet_label = Label(window, text="CZK", bg="black", fg="white", font=("Helvetica", 18, "bold"), borderwidth=5, relief="sunken")
+greet_label.pack()
+
+def change_text():
+    greet_label["text"] = input_1.get()
+    input_1.delete(0, END)
+
+button_1 = Button(text="Click", command = change_text)
+button_1.pack()
+
+input_1 = Entry(width = 20, font=("Helvetica", 18, "bold"))
+input_1.insert(0, string="Mango")
+input_1.focus()
+input_1.pack()
+
+
+
+
+
+# Hlavní cyklus
+window.mainloop()
+
+# 12
