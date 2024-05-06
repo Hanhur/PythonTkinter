a8546f8a4d5cf39c0eec8f0c492f450e1bbba42d
Vladimír Hanhur <aqgz@email.cz>
2024-05-06 15:22:49 +0200

    update

--- Project_1/teacher.py
+++ Project_1/teacher.py
@@ -9,11 +9,26 @@ root.resizable(False, False)
 label_general = Label(root, text = 'Add data')
 label_general.grid(row = 0, column = 1)
 
+# Name ==================================================
 label_name = Label(root, text = 'Name: ')
 label_name.grid(row = 1, column = 0)
 
 entry_name = Entry(root)
 entry_name.grid(row = 1, column = 1)
 
+# Age ===================================================
+label_age = Label(root, text = 'Age: ')
+label_age.grid(row = 2, column = 0)
 
+entry_age = Entry(root)
+entry_age.grid(row = 2, column = 1)
+
+# Address ================================================
+label_address = Label(root, text = 'Address: ')
+label_address.grid(row = 3, column = 0)
+
+entry_address = Entry(root)
+entry_address.grid(row = 3, column = 1)
+
+# Cyclus =================================================
 root.mainloop()
