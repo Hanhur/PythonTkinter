167e3979e58f701b4a7fb5c937f883390894aa95
Vladimír Hanhur <aqgz@email.cz>
2024-05-06 15:44:18 +0200

    update

--- Project_1/teacher.py
+++ Project_1/teacher.py
@@ -6,6 +6,22 @@ root.title('Skola a databaze')
 root.geometry('300x280')
 root.resizable(False, False)
 
+# Functions
+def insert_data(name, age, address):
+    connection = psycopg2.connect(
+        dbname = 'student',
+        user = 'name',
+        password = 'pass',
+        host = '127.0.0.1',
+        port = '5432'
+    )
+
+    cursor = connection.cursor()
+    query = '''INSERT INTO teacher(name, age, address) VALUES (%s, %s, %s)'''
+    cursor.execute(query, (name, age, address))
+    connection.commit()
+    connection.close()
+
 label_general = Label(root, text = 'Add data')
 label_general.grid(row = 0, column = 1)
 
@@ -31,7 +47,7 @@ entry_address = Entry(root)
 entry_address.grid(row = 3, column = 1)
 
 # Button =================================================
-button = Button(root, text = 'Add')
+button = Button(root, text = 'Add', command = lambda:insert_data(entry_name.get(), entry_age.get(), entry_address.get()))
 button.grid(row = 4, column = 1)
 
 # Cyclus =================================================
