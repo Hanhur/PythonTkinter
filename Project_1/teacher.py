2c773be4c35bdfefd8e5ff5bf661fbef8bf8db76
Vladimír Hanhur <aqgz@email.cz>
2024-05-06 14:54:42 +0200

    update

--- Project_1/teacher.py
+++ Project_1/teacher.py
@@ -6,5 +6,14 @@ root.title('Skola a databaze')
 root.geometry('300x280')
 root.resizable(False, False)
 
+label_general = Label(root, text = 'Add data')
+label_general.grid(row = 0, column = 1)
+
+label_name = Label(root, text = 'Name: ')
+label_name.grid(row = 1, column = 0)
+
+entry_name = Entry(root)
+entry_name.grid(row = 1, column = 1)
+
 
 root.mainloop()
