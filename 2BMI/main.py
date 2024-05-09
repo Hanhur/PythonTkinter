3d60a85114db5b266bb37ca915a637141ea12489
Vladimír Hanhur <aqgz@email.cz>
2024-05-09 16:13:26 +0200

    update

--- 2BMI/main.py
+++ 2BMI/main.py
@@ -6,6 +6,24 @@ root.title('Vypocet BMI')
 root.geometry('300x300')
 root.resizable(False, False)
 
+# Functions ===================================================================
+def calculate_bmi(weight, height):
+    text_result = ''
+    bmi = round(float(weight) / float(height) ** 2, 2)
+    if bmi < 18.5:
+        text_result = 'podvaha'
+    elif bmi < 24.9:
+        text_result = 'normalni'
+    elif bmi < 29.2:
+        text_result = 'nadvaha'
+    elif bmi < 34.9:
+        text_result = 'obezita'
+    elif bmi >= 34.9:
+        text_result = 'extremni obezita'
+    
+    label_user_result_1['text'] = bmi
+    label_user_result_2['text'] = text_result
+
 # general label ================================================================
 label_general = Label(root, text = 'Vypocet BMi')
 label_general.grid(row = 0, column = 1)
@@ -25,20 +43,20 @@ entry_height = Entry(root)
 entry_height.grid(row = 2, column = 1)
 
 # Button =======================================================================
-button = Button(root, text = 'Vypocitat')
+button = Button(root, text = 'Vypocitat', command = lambda:calculate_bmi(entry_weight.get(), entry_height.get()))
 button.grid(row = 3, column = 1)
 
 # result section ==============================================================
 label_result_1 = Label(root, text = 'Ciselny vysledek:')
 label_result_1.grid(row = 4, column = 0)
 
-label_user_result_1 = Label(root, text = 'DOPLNIT')
+label_user_result_1 = Label(root)
 label_user_result_1.grid(row = 4, column = 1)
 
 label_result_2 = Label(root, text = 'Textovy vysledek:')
 label_result_2.grid(row = 5, column = 0)
 
-label_user_result_2 = Label(root, text = 'DOPLNIT')
+label_user_result_2 = Label(root)
 label_user_result_2.grid(row = 5, column = 1)
 
 label_count_text = Label(root, text = 'Pocet uzivatelu:')
